76cff9e7b39d536848db8bfe0a14efc2c7a649e6
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-04-20 23:23:11 +0600

    add clock logics

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,37 @@
 import '../scss/main.scss';
+import { Clock } from './Clock/clock';
+import { Util } from './Util/util';
+
+//*================== Variables ======================
+const timeNodes = Object.freeze([
+    Util.$("#hour"),
+    Util.$("#minutes"),
+    Util.$("#seconds"),
+    Util.$("#milliseconds")
+]);
+const languageButton = Util.$("#translateButton > a")
+let language = 'english';
+
+//*================== Functions ======================
+const timeConverter = _ => {
+    const clock = new Clock(language),
+        currentTimes = Object.freeze([
+            clock.getHour,
+            clock.getMinutes,
+            clock.getSeconds,
+            clock.getMilliseconds
+        ]);
+    
+    timeNodes.forEach((time, index) => {
+        time.textContent = currentTimes[index] < 10 ? `0${currentTimes[index]}` : currentTimes[index];
+    })
+}
+
+const changeLanguage = _ => {
+    languageButton.textContent = `translate to ${language}`;
+    language = language === 'english' ? 'bangla' : 'english';
+}
+
+//*================== Invoked ======================
+setInterval(timeConverter, 1);
+languageButton.addEventListener("click", changeLanguage);
