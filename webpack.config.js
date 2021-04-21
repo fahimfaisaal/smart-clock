04091e9a07923bb3601e0b541ac73d5f2dff9d9e
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-04-21 17:50:17 +0600

    modify double quotes to single quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,61 @@
 const path = require('path');
 
-let mode = "development",
-    target = "web";
+let mode = 'development';
+let target = 'web';
 
-if (process.env.NODE_ENV === "production") {
-    mode = "production";
-    target = "browserslist";
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist';
 }
 
-const HtmlWebpackPlugin = require("html-webpack-plugin"),
-    MiniCssExtractPlugin = require("mini-css-extract-plugin"),
-    EslintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    mode: mode,
-    target: target,
-    entry: "./src/js/main.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: "images/[hash][ext][query]",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: "html-loader"
-            }, {
-                test: /\.(s[ac]|c)ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader"
-                ]
-            }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                }
-            }, {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: "asset"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin(),
-        new EslintWebpackPlugin({
-            formatter: 'codeframe',
-            emitError: false,
-            emitWarning: false
-        }),
+  mode,
+  target,
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      }, {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }, {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset',
+      },
     ],
-    devtool:false
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new EslintWebpackPlugin({
+      formatter: 'codeframe',
+      emitError: false,
+      emitWarning: false,
+    }),
+  ],
+  devtool: false,
+};
